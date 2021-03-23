9f9f8d7b1e3938644d90029eec061d564ef5a061
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-03-23 19:39:56 +0000

    sh

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ const express = require('express')
 const app = express()
 let Feed = require('rss-parser')
 let parser = new Feed()
+var all = []
 async function p(){
  
     const now = await parser.parseURL("https://www.saharamedias.net/feed");
     now.items.forEach(item =>{
-        console.log("title: "+item.title);
+        all.push(item)
+        var a = ""+all.find(i=> i == item)
+        if (a.includes("undefined")) {
+            console.log("done");
+           all.push(item)
+        }else{
+            console.log("good");
+            
+        }
         
+        
+        /*
+        if (all.find(item.title) != true) {
+            all.push(item.title)
+            console.log("done");
+            
+        }else{
+            console.log("not done");
+            
+        }*/
+
     })
-    
+
+      console.log(all);
+      
 }
+
 p()
 
 
