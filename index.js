ab2745e6f5a6d2b9095679a1663b90d242a091f8
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-03-24 10:01:33 +0000

    sh

--- index.js
+++ index.js
@@ -2,24 +2,50 @@ const express = require('express')
 const app = express()
 let Feed = require('rss-parser')
 let parser = new Feed()
+const sites = require('./sites.json')
+const cron = require("node-cron")
 var all = []
- async function p(){
- 
-    const now = await parser.parseURL("https://www.saharamedias.net/feed");
+ async function push(){
+for(const site of sites){
+    try{
+    const now = await parser.parseURL(site.url);
     now.items.forEach(item =>{
         all.push(item)
-        var a = ""+all.find(i=> i == item)
-        if (a.includes("undefined")) {
-            console.log("done");
-           all.push(item)
-        }else{
-            console.log("good");
-            
         }
-    })
+    )
+    }catch(e){
+       console.log("I has problem " + e);
+       
+    }
     
+ }
+}
+ 
+ async function update(){
+
+for(const site of sites){
+     const now = await parser.parseURL(site.url);
+    now.items.forEach(ite =>{
+        var a = all.find(i=> i == ite.item)
+        if (a != ite.item) {
+            console.log("done");
+            all.push(ite.item)
+        }
+   })
+ }
 }
 
-p()
 
+async function run(){
+   await push()
+   cron.schedule('* * * * * *', async () => {
+      await update()
+   })
+}
+run()
 
+app.get("/", (req,res)=>{
+    res.send(all)
+})
+
+app.listen(3000, ()=> console.log("in port 3000"))
