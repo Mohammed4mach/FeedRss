0df19ed14c11f3d0734086f2b8196a6660cefd66
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-03-25 19:21:56 +0000

    hff

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ let parser = new Feed()
 const sites = require('./sites.json')
 const cron = require("node-cron")
 const shell = require("shelljs")
+const Domain = require('url-domain-name');
+const titleFromUrl = require('get-title-at-url');
 var all = []
 app.use(cors())
-function add(title, link, date){
+function add(title, link, date, name){
     var post = {
         title: title,
         link: link,
-        date: date
+        isoDate: date,
+        name: name
     } 
     all.push(post)
 }
@@ -20,8 +23,18 @@ function add(title, link, date){
 for(const site of sites){
     try{
     const now = await parser.parseURL(site.url);
+        var nameSite = now.title;
+
     now.items.forEach(item =>{
-        all.push(item)
+        const domain = ""+Domain.from(item.link);
+        
+        titleFromUrl("http://google.com/", function(title){
+               // nameSite = title
+        })
+        console.log(nameSite);
+        
+        add(item.title,item.link,item.isoDate,nameSite)
+        //all.push(item)
         }
     )
     }catch(e){
@@ -31,27 +44,6 @@ for(const site of sites){
     
  }
 }
- /*
- async function update(){
-
-for(const sit of sites){
-    try{
-     const now = await parser.parseURL(sit.url);
-    now.items.forEach(ite =>{
-        var a = all.find(i=> i == ite.title)
-        if (a != ite.title) {
-            all.push(ite)
-            console.log("done " + ite);
-        }
-   })
-    }catch(e){
-        console.log("I has problem in update(): "+e);
-        
-    }
- }
-
-}
-*/
 
 
 async function run(){
@@ -66,9 +58,5 @@ app.get("/", (req,res)=>{
     
     res.send(JSON.stringify(all,null, 3))
 })
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=> cron.schedule('* * * * *', function(){
-     if (shell.exec("node index.js").code !== 0) {
-        console.log("somthing")
-     }
-}))
+const PORT = process.env.PORT || 3000 || 4050
+app.listen(PORT, ()=>console.log("done..."))
