fbe51d3fa424112e8a339c4cc9b683fd9df67407
Sheikh El Moctar El Shengiti <sheikh.el.moctar@gmail.com>
2021-04-01 23:49:41 +0000

    sg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let cors = require("cors")
 let parser = new Feed()
 const sites = require('./sites.json')
 const fetch = require('node-fetch');
-//wake()
+wake()
 
 setInterval(() => {
     
@@ -61,9 +61,9 @@ function wake() {
         .catch(err => console.error(`Error occured: ${err}`))
     }, 1500000);
     setInterval(() => {
-      fetch('https://all-news-mr.herokuapp.com/').then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`))
+      fetch('https://all-news-bot.herokuapp.com/').then(res => console.log(`response-ok: ${res.ok}, status: ${res.status}`))
         .catch(err => console.error(`Error occured: ${err}`))
-    }, 1500000);
+    }, 300000);
 
   } catch(err) {
       console.error('Error occured: retrying...')
